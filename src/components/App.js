95e0969df2b34d88f82ca62034196656adc13069
Drizz159 <celnar.lukas@gmail.com>
2020-09-14 22:18:33 +0200

    Adding Kontakt and Pdf components

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import Info from './Info/Info';
 import Poloha from './Poloha/Poloha';
 import Video from './Video/Video';
+import Kontakt from './Kontakt/Kontakt';
+import Pdf from './Pdf/Pdf';
 import './App.scss';
+
+// root route components combined together
+const MainPage = () => {
+	return (
+		<>
+			<Info />
+			<Poloha />
+			<Video />
+			<Kontakt />
+		</>
+	);
+}
 
 const App = () => {
 	return (
 		<Router>
 			<Navbar />
-			<Route path='/' exact component={Info} />
-			<Route path='/' exact component={Poloha} />
-			<Route path='/' exact component={Video} />
+			<Switch>
+				<Route path='/' exact component={MainPage} />
+				<Route path='/karta' exact>
+					<Pdf pdfPath='/pdfs/karta.pdf'/>
+				</Route>
+			</Switch>
 		</Router>
 	);
 };
