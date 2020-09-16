40c5d1f38353f22e6dc0d12e49159b5bb7720f23
Drizz159 <celnar.lukas@gmail.com>
2020-09-16 19:39:20 +0200

    Finishing Vizualizace component

--- src/components/Vizualizace/Vizualizace.js
+++ src/components/Vizualizace/Vizualizace.js
@@ -4,14 +4,14 @@ import './Vizualizace.scss';
 const Vizualizace = () => {
     return (
         <div className='vizualizace'>
-            <img className='vizualizace__img-1' src='/imgs/viz1.jpg' alt='viz1' />
-            <div className='vizualizace__header-1'>Vizualizace</div>
-            <img className='vizualizace__img-2' src='/imgs/viz2.jpg' alt='viz2' />
-            <img className='vizualizace__img-3' src='/imgs/viz3.jpg' alt='viz3' />
-            <img className='vizualizace__img-4' src='/imgs/viz4.jpg' alt='viz4' />
-            <div className='vizualizace__header-2'>Model</div>
-            <img className='vizualizace__img-5' src='/imgs/viz5.jpg' alt='viz5' />
-            <img className='vizualizace__img-6' src='/imgs/viz6.jpg' alt='viz6' />
+            <div className='vizualizace__div-1'><img className='vizualizace__img' src='/imgs/viz1.jpg' alt='viz1' /></div>
+            <div className='vizualizace__div-2 vizualizace__header-1'><div>Vizualizace</div></div>
+            <div className='vizualizace__div-3'><img className='vizualizace__img' src='/imgs/viz2.jpg' alt='viz2' /></div>
+            <div className='vizualizace__div-4'><img className='vizualizace__img' src='/imgs/viz3.jpg' alt='viz3' /></div>
+            <div className='vizualizace__div-5'><img className='vizualizace__img' src='/imgs/viz4.jpg' alt='viz4' /></div>
+            <div className='vizualizace__div-6 vizualizace__header-2'><div>Model</div></div>
+            <div className='vizualizace__div-7'><img className='vizualizace__img' src='/imgs/viz5.jpg' alt='viz5' /></div>
+            <div className='vizualizace__div-8'><img className='vizualizace__img' src='/imgs/viz6.jpg' alt='viz6' /></div>
         </div>
     );
 };
