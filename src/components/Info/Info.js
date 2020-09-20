a6518a9b6403e9963ec3184f24c9d561d081e4c1
Drizz159 <celnar.lukas@gmail.com>
2020-09-20 20:45:04 +0200

    Adding item in Info specs and fixing typo

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -14,18 +14,20 @@ const Info = () => {
                             <div>Podlaží</div>
                             <div>Užitná plocha</div>
                             <div>Celková plocha</div>
-                            <div>Terasa</div>
                             <div>Balkon</div>
+                            <div>Terasa</div>
                             <div>Sklepní koje</div>
+                            <div>Garážové stání</div>
                             <div>Nastěhování</div>
                         </div>
                         <div className='info__specs-content-right-column' style={{width: '35%'}}>
                             <div>8.NP a 9.NP</div>
                             <div>106,6 m²</div>
-                            <div>119,8 m²</div>
-                            <div>31,4 m²</div>
+                            <div>115,5 m²</div>
                             <div>20,3 m²</div>
+                            <div>31,4 m²</div>
                             <div>Ano</div>
+                            <div>Ano 2x</div>
                             <div>Podzim 2022</div>
                         </div>
                     </div>
