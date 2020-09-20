860258a16f1e4437c2447de2812c9a07ec58e4c4
Drizz159 <celnar.lukas@gmail.com>
2020-09-20 21:06:07 +0200

    Fixing title in Video and image names in public/imgs folder

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,7 +4,7 @@ import './Video.scss'
 const Video = () => {
     return (
         <div className='video' id='video'>
-            <div className='video__header'>Video Prohlídka</div>
+            <div className='video__header'>Video okolí</div>
             <iframe className='video__video-player' title='video' id="ytplayer" type="text/html" width="1020" height="605" src="https://www.youtube.com/embed/3XqS8kB5_rg" frameBorder="0" allowFullScreen="allowfullscreen"></iframe>
         </div>
     );
