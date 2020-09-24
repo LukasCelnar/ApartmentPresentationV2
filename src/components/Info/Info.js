7b56991862935416f688af316e0231723e9118f6
Drizz159 <celnar.lukas@gmail.com>
2020-09-24 12:38:25 +0200

    Adding B4/02.05 project

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -4,7 +4,7 @@ import './Info.scss';
 const Info = () => {
     return (
         <div className='info' id='info'>
-           <div className='info__header'>PALÁC TRNITÁ - STŘEŠNÍ BYT B6/08.04</div>
+           <div className='info__header'>PALÁC TRNITÁ - BYT S TERASOU B4/02.05</div>
            <div className='info__content'>
                 <img className='info__img info__img-left' alt='main-picutre1' src='/imgs/main1.jpg'/>
                 <div className='info__specs'>
@@ -14,24 +14,22 @@ const Info = () => {
                             <div>Podlaží</div>
                             <div>Užitná plocha</div>
                             <div>Celková plocha</div>
-                            <div>Balkon</div>
                             <div>Terasa</div>
-                            <div>Sklepní koje</div>
+                            <div>Sklepní kóje</div>
                             <div>Garážové stání</div>
-                            <div>Nastěhování</div>
+                            <div>K nastěhování</div>
                         </div>
                         <div className='info__specs-content-right-column' style={{width: '35%'}}>
-                            <div>8.NP a 9.NP</div>
-                            <div>106,6 m²</div>
-                            <div>115,5 m²</div>
-                            <div>20,3 m²</div>
-                            <div>31,4 m²</div>
+                            <div>2.NP</div>
+                            <div>74,5 m²</div>
+                            <div>79,1 m²</div>
+                            <div>27,9 m²</div>
                             <div>Ano</div>
-                            <div>Ano 2x</div>
+                            <div>Ano 1x</div>
                             <div>Podzim 2022</div>
                         </div>
                     </div>
-                    <div className='info__specs-footer'>12 699 000 Kč</div>
+                    <div className='info__specs-footer'>8 299 000 Kč</div>
                 </div>
                 <img className='info__img info__img-right' alt='main-picture2' src='/imgs/main2.jpg'/>
            </div>
