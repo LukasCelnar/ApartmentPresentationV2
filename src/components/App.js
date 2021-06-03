9df35d43e3e8b1522f62c6eb7cc378ee0a15ea1e
Lukáš Celnar <58079645+LukasCelnar@users.noreply.github.com>
2021-06-03 14:30:23 +0200

    Removing Redirect import

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import Info from './Info/Info';
 import Poloha from './Poloha/Poloha';
